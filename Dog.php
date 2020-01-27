60989cdb2fb2f8319f756578c77eafd38d3d93dd
Nicolas Rellier <nicolasrellier@yahoo.fr>
2020-01-27 14:26:57 +0100

    feat #1 (realease-1.0) : new review changes done

--- Dog.php
+++ Dog.php
@@ -1,5 +1,4 @@
 <?php
-
 class Dog
 {
     private $name;
@@ -8,63 +7,112 @@ class Dog
     private $weight;
     private const SPEEDING = 60;
 
-    public function __construct($name = '', $race = '', $age = 0, $weight = 15)
+    public function __construct($name = '', $race = '', $age , $weight = 15)
     {
-        echo 'Vous avez adoptez un nouveau '.strtolower(get_class()).'<br>';
         $this->name = $name;
         $this->race = $race;
-        $this->$age = $age;
+        $this->age = $age;
         $this->weight = $weight;
+        echo 'Vous avez adoptez un nouveau '.strtolower(get_class()).'.<br>'; //Il s\'appelle '.$this->name.' et pèse '.$this->weight.'kgs. Il s\'agit d\'un '.$this->race.'. Il est agé de '.$this->age.'<br>';
     }
 
     public function getName()
     {
+        if(empty($this->name))
+        {
+            return 'Le nom  \'est pas encore défini. <br>';
+        }
         return $this->name;
     }
 
     public function getRace()
     {
+        if(empty($this->race))
+        {
+            return 'La race n\'est pas encore définie. <br>';
+        }
         return $this->race;
     }
 
     public function getAge()
     {
+        if(empty($this->age))
+        {
+            return 'L\'age  n\'est pas encore défini. <br>';
+        }
         return $this->age;
     }
 
     public function getWeight()
     {
+        if(empty($this->weight))
+        {
+            return 'Le poid du chien  n\'est pas encore défini. <br>';
+        }
         return $this->weight;
     }
 
     public function setName($name)
     {
+        if(!is_string($name))
+        {
+            trigger_error('Veuillez définir le nom avec des lettres. <br>');
+            return;
+        }
         $this->name = $name;
     }
 
     public function setRace($race)
     {
+        if(!is_string($race))
+        {
+            trigger_error('Veuillez définir la race avec des lettre. <br>');
+            return;
+        }
         $this->race = $race;
     }
 
     public function setAge($age)
     {
+        if(!is_numeric($age))
+        {
+            trigger_error('Veuillez renseigner un nombre pour définir l\'age.<br>');
+            return;
+        }
         $this->age = $age;
     }
 
     public function setWeight($weight)
     {
+        if(!is_numeric($weight))
+        {
+            trigger_error('Veuillez renseigner un nombre pour définir le poids.<br>');
+            return;
+        }
         $this->weight = $weight;
     }
 
     public function eating($quantity)
     {
-        $newWeight += $this->getWeight() + $quantity;
+        if(!is_numeric($quantity))
+        {
+            trigger_error('Veuillez renseigner un nombre pour définir la quantité de nourriture mangé.<br>');
+        }
+
+        if(0 == $this->getWeight())
+        {
+            $newWeight = 0;
+        }
+        else
+        {
+            $newWeight = $this->getWeight();
+        }
+        $newWeight += $quantity;
         $this->setWeight($newWeight);
     }
 
     public function running()
     {
-        return 'Le chien court à une vitesse de '.self::SPEEDING.'km/h';
+        return 'Le chien court à une vitesse de '.self::SPEEDING.'km/h<br>';
     }
 }
--- index.php
+++ index.php
@@ -2,25 +2,48 @@
 require_once 'Cat.php';
 require_once 'Dog.php';
 
-$garfield = new Cat();
-echo $garfield->getName();
-echo 'Le chat pèse '.$garfield->getWeight().'kgs.<br>';
+$garfield = new Cat('garfield', 'chat de goutière', 5);
+echo 'Le chat s\'appelle '.$garfield->getName().'. Il pèse '.$garfield->getWeight().'kgs. Il s\'agit d\'un '.$garfield->getRace().'. Il est agé de '.$garfield->getAge().'ans<br>';
 
 $garfield->eating(6);
 echo $garfield->getWeight().'<br>';
-
 echo $garfield->running().'<br>';
 
 
-$beethoven = new Dog();
-echo $beethoven->getName();
-echo 'Le chien pèse '.$beethoven->getWeight().'kgs.<br>';
-
-$beethoven->eating(12);
+$beethoven = new Dog('beethoven', 'caniche', 1);
+echo 'Le chien s\'appelle '.$beethoven->getName().'. Il pèse '.$beethoven->getWeight().'kgs. Il s\'agit d\'un '.$beethoven->getRace().'. Il est agé de '.$beethoven->getAge().'ans<br>';
+$beethoven->eating(10).'<br>';
 echo $beethoven->getWeight().'<br>';
 
 echo $beethoven->running().'<br>';
 
 $idefix = new Dog('idefix', 'westie', 2);
-echo $idefix->getName().' '.$idefix->getRace().' '.$idefix->getAge.''.$idefix->getWeight();
+echo 'Le chien s\'appelle '.$beethoven->getName().'. Il pèse '.$beethoven->getWeight().'kgs. Il s\'agit d\'un '.$beethoven->getRace().'. Il est agé de '.$beethoven->getAge().'ans<br>';
 
+
+echo '<br>Modification des propriété d\'un chat avec paramètre non attendus :';
+$garfield->setName(456);
+echo $garfield->getName();
+
+$garfield->setRace(123);
+echo $garfield->getRace();
+
+$garfield->setAge('wamiz');
+echo $garfield->getAge();
+
+$garfield->setWeight('bengal');
+echo $garfield->getWeight();
+
+
+echo '<br>Modification des propriété d\'un chien avec paramètre non attendus :';
+$idefix->setName(456);
+echo $idefix->getName();
+
+$idefix->setRace(123);
+echo $idefix->getRace();
+
+$idefix->setAge('medor');
+echo $idefix->getAge();
+
+$idefix->setWeight('labrador');
+echo $idefix->getWeight();
